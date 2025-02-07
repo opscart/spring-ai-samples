412948e87c05135bb01f852d5899a1c00b228efa
danvega <danvega@gmail.com>
2025-02-07 14:58:18 -0500

    Updating package name

--- ai-chat-demo/src/main/java/com/example/aichatdemo/AiChatApplication.java
+++ ai-chat-demo/src/main/java/com/example/aichatdemo/AiChatApplication.java
@@ -1,4 +1,4 @@
-package com.example.ai_chat_demo;
+package com.example.aichatdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- ai-chat-demo/src/test/java/com/example/aichatdemo/AiChatApplicationTests.java
+++ ai-chat-demo/src/test/java/com/example/aichatdemo/AiChatApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ai_chat_demo;
+package com.example.aichatdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
